3ed31d04cd2f96579a6ad30e73198609b8f2624f
Lawrence Matlala <777thami@gmail.com>
2025-10-06 11:28:59 +0200

    Updating the landing page to be the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopBar from "./components/topbar.jsx"; // points to your components folder
 import SideNavBar from "./components/sideNavBar.jsx";
 import Home from "./pages/home.jsx";
 import About from "./pages/about.jsx";
 import Prices from "./pages/prices.jsx";
 import Contact from "./pages/contact.jsx";
-
+/*
 function App() {
   return (
     <Router>
@@ -21,5 +21,24 @@ function App() {
     </Router>
   );
 }
+*/
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter basename="/latiroWebsite/">
+        <TopBar />
+        <SideNavBar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="prices" element={<Prices />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+//export default App;
