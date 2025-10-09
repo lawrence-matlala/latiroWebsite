fcbed22d22a3081f271cf4ca63b0fb4a11f015db
Lawrence Matlala <777thami@gmail.com>
2025-10-09 14:13:47 +0200

    Testing the left and the right arrows on the carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -12,6 +12,20 @@ const carouselImages = [latImage1, latImage2, latImage3, latImage4, latImage5];
 function Carousel() {
   const [imageIndex, setImageIndex] = useState(0);
 
+  function showNextImage() {
+    setImageIndex((index) => {
+      if (index === carouselImages.length - 1) return 0;
+      return index + 1;
+    });
+  }
+
+  function showPrevImage() {
+    setImageIndex((index) => {
+      if (index === 0) return carouselImages.length - 1;
+      return index - 1;
+    });
+  }
+
   return (
     <div style={{ width: "100%", height: "100%", position: "relative" }}>
       <img
@@ -19,10 +33,18 @@ function Carousel() {
         alt="carousel"
         className="carousel-test-image"
       />
-      <button className="img-slider-btn" style={{ left: 0 }}>
+      <button
+        onClick={showPrevImage}
+        className="img-slider-btn"
+        style={{ left: 0 }}
+      >
         <ArrowBigLeft />
       </button>
-      <button className="img-slider-btn" style={{ right: 0 }}>
+      <button
+        onClick={showNextImage}
+        className="img-slider-btn"
+        style={{ right: 0 }}
+      >
         <ArrowBigRight />
       </button>
     </div>
